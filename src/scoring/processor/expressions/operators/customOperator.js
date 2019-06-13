7e4c1f2990949328b3f9bfe1ba8d31119dd0e170
Martin Nicholson <martin@taotesting.com>
2019-06-13 10:25:07 +0200

    Update all copyright years to 20xx-2019

--- src/scoring/processor/expressions/operators/customOperator.js
+++ src/scoring/processor/expressions/operators/customOperator.js
@@ -13,7 +13,7 @@
  * along with this program; if customOperator, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015 (original work) Open Assessment Technlogies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2015-2019 (original work) Open Assessment Technlogies SA (under the project TAO-PRODUCT);
  *
  */
 
