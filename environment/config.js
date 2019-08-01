c8fa16e149b217b6b87aeef205c050dd156a8b76
Tamas Besenyei <tamas@besenyei.net>
2019-08-01 14:23:34 +0200

    update tao core libs and integrate qunit testrunner

--- environment/config.js
+++ environment/config.js
@@ -16,90 +16,75 @@
  * Copyright (c) 2019 (original work) Open Assessment Technologies SA ;
  */
 
-requirejs.config({
-    baseUrl: '/',
-    paths: {
-        css: '/node_modules/require-css/css',
-        json: '/node_modules/requirejs-plugins/src/json',
-        text: '/node_modules/requirejs-plugins/lib/text',
+define(['/node_modules/@oat-sa/tao-core-libs/dist/pathdefinition.js'], function(libPathDefinition) {
+    requirejs.config({
+        baseUrl: '/',
+        paths: Object.assign({}, {
+            css: '/node_modules/require-css/css',
+            json: '/node_modules/requirejs-plugins/src/json',
+            text: '/node_modules/requirejs-plugins/lib/text',
 
-        /* TEST related */
-        'qunit-parameterize': '/environment/qunit2-parameterize',
-        'qunit-assert-close': '/node_modules/qunit-assert-close/qunit-assert-close',
-        qunit: '/node_modules/qunit/qunit/qunit',
-        qunitStyle: '/node_modules/qunit/qunit/qunit',
-        'taoQtiItem/test': '/test',
+            /* TEST related */
+            'qunit-parameterize': '/environment/qunit2-parameterize',
+            'qunit-assert-close': '/node_modules/qunit-assert-close/qunit-assert-close',
+            qunit: '/node_modules/qunit/qunit/qunit',
+            qunitStyle: '/node_modules/qunit/qunit/qunit',
+            'taoQtiItem/test': '/test',
 
-        taoQtiItem: '/dist',
-        core: '/node_modules/@oat-sa/tao-core-sdk/dist/core',
-        util: '/node_modules/@oat-sa/tao-core-sdk/dist/util',
-        ui: '/node_modules/@oat-sa/tao-core-ui/dist',
-        lib: '/node_modules/@oat-sa/tao-core-libs/dist',
-        'taoItems/runner': '/node_modules/@oat-sa/tao-item-runner/dist/runner',
-        'taoItems/assets': '/node_modules/@oat-sa/tao-item-runner/dist/assets',
-        'taoItems/scoring': '/node_modules/@oat-sa/tao-item-runner/dist/scoring',
-        jquery: '/node_modules/jquery/jquery',
-        lodash: '/node_modules/lodash/lodash',
-        moment: '/node_modules/moment/min/moment-with-locales',
-        handlebars: '/node_modules/handlebars/dist/handlebars.amd',
+            taoQtiItem: '/dist',
+            core: '/node_modules/@oat-sa/tao-core-sdk/dist/core',
+            util: '/node_modules/@oat-sa/tao-core-sdk/dist/util',
+            ui: '/node_modules/@oat-sa/tao-core-ui/dist',
+            'taoItems/runner': '/node_modules/@oat-sa/tao-item-runner/dist/runner',
+            'taoItems/assets': '/node_modules/@oat-sa/tao-item-runner/dist/assets',
+            'taoItems/scoring': '/node_modules/@oat-sa/tao-item-runner/dist/scoring',
 
-        /* LIBS */
-        'lib/popper/tooltip': '/node_modules/tooltip.js/dist/umd/tooltip',
-        popper: '/node_modules/popper.js/dist/umd/popper',
-        select2: '/node_modules/select2/select2',
-        interact: '/node_modules/interactjs/dist/interact',
-        'lib/dompurify/purify': '/node_modules/dompurify/dist/purify',
-        'lib/gamp/gamp': '/node_modules/gamp/src/gamp',
-        'lib/simulator': '/lib/simulator',
-        iframeNotifier: '/lib/iframeNotifier',
-        class: 'lib/class',
-        async: '/node_modules/async/lib/async',
-        nouislider: '/lib/sliders/jquery.nouislider',
-        ckeditor: '/lib/ckeditor/ckeditor',
-        raphael: '/node_modules/raphael/raphael',
-        'scale.raphael': '/lib/raphael/scale.raphael'
+            /* LIBS */
+            'lib/simulator': '/lib/simulator',
+            ckeditor: '/lib/ckeditor/ckeditor'
         /* LIBS END */
-    },
-    shim: {
-        'qunit-parameterize': {
-            deps: ['qunit']
+        }, libPathDefinition),
+        shim: {
+            'qunit-parameterize': {
+                deps: ['qunit']
+            },
+            'qunit-assert-close': {
+                deps: ['qunit']
+            },
+            ckeditor: {
+                exports: 'CKEDITOR'
+            }
         },
-        'qunit-assert-close': {
-            deps: ['qunit']
+        waitSeconds: 15
+    });
+
+    define('qunitLibs', ['qunit', 'css!qunitStyle']);
+    define('qunitEnv', ['qunitLibs', 'qunit-parameterize'], function() {
+        requirejs.config({ nodeIdCompat: true });
+    });
+
+    define('context', ['module'], function(module) {
+        return module.config();
+    });
+
+    define('i18n', [], () => text => text);
+
+    define('taoQtiItem/portableElementRegistry/ciRegistry', [], () => ({
+        resetProviders() {
+            throw new Error('Not implemented');
         },
-        ckeditor: {
-            exports: 'CKEDITOR'
+        registerProvider() {
+            throw new Error('Not implemented');
         }
-    },
-    waitSeconds: 15
+    }));
+    define('taoQtiItem/portableElementRegistry/icRegistry', [], () => ({
+        resetProviders() {
+            throw new Error('Not implemented');
+        },
+        registerProvider() {
+            throw new Error('Not implemented');
+        }
+    }));
+    define('taoQtiItem/portableElementRegistry/provider/sideLoadingProviderFactory', [], () => {});
+    define('taoQtiItem/portableElementRegistry/assetManager/portableAssetStrategy', [], () => {});
 });
-
-define('qunitLibs', ['qunit', 'css!qunitStyle']);
-define('qunitEnv', ['qunitLibs', 'qunit-parameterize'], function() {
-    requirejs.config({ nodeIdCompat: true });
-});
-
-define('context', ['module'], function(module) {
-    return module.config();
-});
-
-define('i18n', [], () => text => text);
-
-define('taoQtiItem/portableElementRegistry/ciRegistry', [], () => ({
-    resetProviders() {
-        throw new Error('Not implemented');
-    },
-    registerProvider() {
-        throw new Error('Not implemented');
-    }
-}));
-define('taoQtiItem/portableElementRegistry/icRegistry', [], () => ({
-    resetProviders() {
-        throw new Error('Not implemented');
-    },
-    registerProvider() {
-        throw new Error('Not implemented');
-    }
-}));
-define('taoQtiItem/portableElementRegistry/provider/sideLoadingProviderFactory', [], () => {});
-define('taoQtiItem/portableElementRegistry/assetManager/portableAssetStrategy', [], () => {});
