e00f2ec277df2dbcedc0082af90a2b70f24ff879
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-04-07 14:02:54 +0200

    Fix manual error

--- src/qtiCommonRenderer/renderers/interactions/TextEntryInteraction.js
+++ src/qtiCommonRenderer/renderers/interactions/TextEntryInteraction.js
@@ -132,7 +132,7 @@ var render = function render(interaction) {
 
             hideTooltip($input);
 
-            if (($input.val().length && regex.test($input.val())) || ($input.val().length && regex.test($input.val()))) {
+            if ($input.val().length && regex.test($input.val()) || $input.val().length === 0 && regex.test($input.val())) {
                 $input.removeClass('invalid');
             } else {
                 $input.addClass('invalid');
