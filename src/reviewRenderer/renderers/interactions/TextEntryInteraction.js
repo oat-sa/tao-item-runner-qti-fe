c451880a4481fcedfcbb7c74b295610f14e02979
anastasia <nastya.razum96@gmail.com>
2020-11-19 19:53:09 +0300

    Replace input to div

--- src/reviewRenderer/renderers/interactions/TextEntryInteraction.js
+++ src/reviewRenderer/renderers/interactions/TextEntryInteraction.js
@@ -17,10 +17,145 @@
  *
  */
 
-/**
- * @author Ansul Sharma <ansultaotesting.com>
- */
-import template from 'taoQtiItem/reviewRenderer/tpl/interactions/textEntryInteraction';
-import textEntryInteraction from 'taoQtiItem/qtiCommonRenderer/renderers/interactions/TextEntryInteraction';
+import _ from 'lodash';
+import tpl from 'taoQtiItem/reviewRenderer/tpl/interactions/textEntryInteraction';
+import containerHelper from 'taoQtiItem/qtiCommonRenderer/helpers/container';
+import pciResponse from 'taoQtiItem/qtiCommonRenderer/helpers/PciResponse';
+import locale from 'util/locale';
 
-export default Object.assign({}, textEntryInteraction, {template});
+/**
+ * Init rendering, called after template injected into the DOM
+ * All options are listed in the QTI v2.1 information model:
+ * http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10333
+ *
+ * @param {object} interaction
+ */
+const render = function render(interaction) {
+    let attributes = interaction.getAttributes(),
+        $input = interaction.getContainer(),
+        expectedLength;
+    //setting up the width of the input field
+    if (attributes.expectedLength) {
+        //adding 2 chars to include reasonable padding size
+        expectedLength = parseInt(attributes.expectedLength) + 2;
+        $input.css('width', expectedLength + 'ch');
+        $input.css('min-width', expectedLength + 'ch');
+    }
+};
+
+const resetResponse = function resetResponse(interaction) {
+    interaction.getContainer().text('');
+};
+
+/**
+ * Set the response to the rendered interaction.
+ *
+ * The response format follows the IMS PCI recommendation :
+ * http://www.imsglobal.org/assessment/pciv1p0cf/imsPCIv1p0cf.html#_Toc353965343
+ *
+ * Available base types are defined in the QTI v2.1 information model:
+ * http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10333
+ *
+ * Special value: the empty object value {} resets the interaction responses
+ *
+ * @param {object} interaction
+ * @param {object} response
+ */
+const setResponse = function setResponse(interaction, response) {
+    let responseValue;
+
+    try {
+        responseValue = pciResponse.unserialize(response, interaction);
+    } catch (e) {}
+
+    if (responseValue && responseValue.length) {
+        interaction.getContainer().text(responseValue[0]);
+    }
+};
+
+/**
+ * Return the response of the rendered interaction
+ *
+ * The response format follows the IMS PCI recommendation :
+ * http://www.imsglobal.org/assessment/pciv1p0cf/imsPCIv1p0cf.html#_Toc353965343
+ *
+ * Available base types are defined in the QTI v2.1 information model:
+ * http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10333
+ *
+ * @param {object} interaction
+ * @returns {object}
+ */
+const getResponse = function getResponse(interaction) {
+    const ret = { base: {} },
+        $input = interaction.getContainer(),
+        attributes = interaction.getAttributes(),
+        baseType = interaction.getResponseDeclaration().attr('baseType'),
+        numericBase = attributes.base || 10;
+    let value;
+
+    if ($input.hasClass('invalid') || (attributes.placeholderText && $input.text() === attributes.placeholderText)) {
+        //invalid response or response equals to the placeholder text are considered empty
+        value = '';
+    } else {
+        if (baseType === 'integer') {
+            value = locale.parseInt($input.text(), numericBase);
+        } else if (baseType === 'float') {
+            value = locale.parseFloat($input.text());
+        } else if (baseType === 'string') {
+            value = $input.text();
+        }
+    }
+
+    ret.base[baseType] = isNaN(value) && typeof value === 'number' ? '' : value;
+
+    return ret;
+};
+
+const destroy = function destroy(interaction) {
+    //remove all references to a cache container
+    containerHelper.reset(interaction);
+};
+
+/**
+ * Set the interaction state. It could be done anytime with any state.
+ *
+ * @param {Object} interaction - the interaction instance
+ * @param {Object} state - the interaction state
+ */
+const setState = function setState(interaction, state) {
+    if (_.isObject(state)) {
+        if (state.response) {
+            interaction.resetResponse();
+            interaction.setResponse(state.response);
+        }
+    }
+};
+
+/**
+ * Get the interaction state.
+ *
+ * @param {Object} interaction - the interaction instance
+ * @returns {Object} the interaction current state
+ */
+const getState = function getState(interaction) {
+    const state = {};
+    const response = interaction.getResponse();
+
+    if (response) {
+        state.response = response;
+    }
+    return state;
+};
+
+export default {
+    qtiClass: 'textEntryInteraction',
+    template: tpl,
+    render: render,
+    getContainer: containerHelper.get,
+    setResponse: setResponse,
+    getResponse: getResponse,
+    resetResponse: resetResponse,
+    destroy: destroy,
+    setState: setState,
+    getState: getState
+};
