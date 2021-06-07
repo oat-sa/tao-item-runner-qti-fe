138e65890a1a2080cc10c75d58426863e912debe
AnastasiaArcadia <nastya.razum96@gmail.com>
2021-06-08 01:13:33 +0300

    feat: make inline choice a container

--- src/qtiItem/core/choices/InlineChoice.js
+++ src/qtiItem/core/choices/InlineChoice.js
@@ -1,5 +1,7 @@
 import QtiTextVariableChoice from 'taoQtiItem/qtiItem/core/choices/TextVariableChoice';
+import Container from 'taoQtiItem/qtiItem/mixin/ContainerInline';
 var QtiInlineChoice = QtiTextVariableChoice.extend({
     qtiClass: 'inlineChoice'
 });
+Container.augment(QtiInlineChoice);
 export default QtiInlineChoice;
