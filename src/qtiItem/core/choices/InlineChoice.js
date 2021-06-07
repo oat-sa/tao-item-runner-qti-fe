03294fa7d5b58f51b802527a2bbfaa4daad6fbc7
AnastasiaArcadia <nastya.razum96@gmail.com>
2021-06-08 02:51:01 +0300

    fix: remove unused

--- src/qtiItem/core/choices/InlineChoice.js
+++ src/qtiItem/core/choices/InlineChoice.js
@@ -1,7 +1,5 @@
 import QtiContainerChoice from 'taoQtiItem/qtiItem/core/choices/ContainerChoice';
-import Container from 'taoQtiItem/qtiItem/mixin/ContainerInline';
 var QtiInlineChoice = QtiContainerChoice.extend({
     qtiClass: 'inlineChoice'
 });
-Container.augment(QtiInlineChoice);
 export default QtiInlineChoice;
